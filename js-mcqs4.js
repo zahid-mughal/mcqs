b11be9690ee2f2b329302a3d5f8cf0da26982139
zahidmughal <zahisohna@gmail.com>
2020-12-14 13:14:02 -0800

    add arrays of objects for questions

--- js-mcqs4.js
+++ js-mcqs4.js
@@ -1,4 +1,48 @@
-function anchor()
+// function anchor()
+// {
+//     location.href="index-mcqs4-main.html";
+// }
+let questions =[
+    //first Index 
+    { 
+    //first object 
+        id:1,
+    
+     //2nd object
+        question: "what is the input device?",
+    //3rd object
+        answer: "Keyboard",
+       
+     //4th object
+        options:[ "Monitor",
+                  "Headphone",
+                  "LCD",
+                  "Keyboard"
+                ]
+
+},
+//2nd Index
 {
-    location.href="index-mcqs4-main.html";
+    id=2,
+    question:"what is the output device?",
+    answer:"Lcd",
+    options:["Mouse",
+             "Lcd",
+             "Headphone",
+             "Keyboard"
+            ]
+
+},
+
+{
+    id=3,
+    question:"What is RAM stand for?",
+    answer:"Random Acces Memmory",
+    options:["Read Acces Memmory",
+             "Read Absolute Memmory",
+             "Random Acces Memmory",
+             "Random Arise Memmory"
+            ]
+
 }
+];
